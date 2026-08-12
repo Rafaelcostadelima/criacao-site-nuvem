1bcdd4845ee27918deacddbf5863def80940936a
mari <mariana.casilva0633@gmail.com>
2026-08-12 17:24:18 -0300

    Update mari primeira pagina e imagens

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+    <body>
+        <!-- essa section é meio que um cabeçalho bem porco mas pode tirar se quiser -->
+        <section>
+            <img src="img/angrybirds.jfif" alt="">
+            <h1>Angry Birds</h1>
+            <h2>Jogos e filmes</h2>
+        </section>
+        <section>
+            <div>
+                <h1>Primeiro Jogo</h1>
+                <p>Angry Birds é um jogo eletrônico de quebra-cabeça desenvolvido pela desenvolvedora finlandesa de jogos: Rovio Entertainment, este foi o primeiro jogo da série de jogos de Angry Birds. Inspirado principalmente por um esboço de pássaros desenhados sem asas, o jogo foi lançado para iOS e Maemo em 11 de dezembro de 2009. Desde então, mais de 12 milhões de cópias do jogo foram compradas para iOS na App Store, o que levou a empresa a projetar versões do jogo para outras plataformas móveis, principalmente para as plataformas Android, Symbian, Windows Phone e BlackBerry 10. Desde então, a série se expandiu para incluir jogos para consoles de videogame e para PCs e jogos online de navegador. 
+                </p>
+            </div>  
+            <div>
+                <h2>Modo de Jogo</h2>
+                <p>
+                    No jogo, os jogadores usam um estilingue para lançar pássaros em porcos localizados em torno de várias estruturas, com o objetivo de eliminar todos os porcos do nível. Ao longo do jogo é possível desbloquear novos tipos de pássaros e alguns deles tem habilidades especiais que podem ser ativadas pelo jogador, ao clicar na tela. Rovio Entertainment também lançou no jogo Angry Birds, inúmeras atualizações gratuitas que incluem novos níveis.
+                </p>
+                <img src="img/gameplay.gif" alt="">
+            </div>      
+            <div>
+                <h2>Personagens</h2>
+                <img src="img/angrybirds.png" alt="">
+                <li>
+                    <ul>Red - Contribuição na destruição (Red é o líder do bando).</ul>
+                    <ul>Chuck - Super velocidade (melhor com madeira).</ul>
+                    <ul>Azuis ou Jim, Jake e Jay - Multiplicação em três (melhor com vidro).</ul>
+                    <ul>Bomba - Explosão (recomendado para pedra).</ul>
+                    <ul>Matilda - Atirar ovos.</ul>
+                    <ul>Terêncio - Super-força.</ul>
+                    <ul>Hal - Vai girando e volta ao contrário como um bumerangue.</ul>
+                    <ul>Bubu - Inflar o corpo.</ul>
+                    <ul>Stella - Criar bolhas que fazem itens flutuarem.</ul>
+                </li>                       
+            </div>
+        </section>   
+    </body>
+?>
